3e4134c9b4b3d88e3f307b2c8b067767be036f21
HARSHEET ABHIJEET GUNDECHA <harsheetgundecha23@gmail.com>
2025-01-29 19:43:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,14 @@ MASK_THRESHOLD = 0.6
 OUTPUT_FPS = 30
 
 # --- Function Definitions ---
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 def convert_to_png(image_path, output_dir="."):
     """Converts an image to PNG and returns the new path."""
     try:
